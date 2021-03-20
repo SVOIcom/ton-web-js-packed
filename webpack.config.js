d9a3aa22bffbdc6fb80d15c651ad0c7652f7e765
Andrey Nedobylsky <andrey@izzz.io>
2021-03-20 17:19:56 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     plugins: [
        /* new CopyPlugin({
             patterns: [
